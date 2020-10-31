a63610085795ed8c013fa3c36858718e4e7bc4cd
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2020-10-31 17:32:20 +0530

    Admin Logout implemented

--- app/src/main/java/being/test/app/DashboardHome.kt
+++ app/src/main/java/being/test/app/DashboardHome.kt
@@ -4,17 +4,37 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.GridLayout
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.afollestad.materialdialogs.MaterialDialog
+import com.google.firebase.auth.FirebaseAuth
 
 class DashboardHome : AppCompatActivity(), View.OnClickListener {
 
     lateinit var userAccType: String
+    lateinit var logoutButtonDash : ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard_home_layout)
         userAccType = PrefUtils.getString(this, PrefKeys.USER_ACC_TYPE, null)
+        logoutButtonDash = findViewById(R.id.logoutButtonDash) as ImageButton
+        logoutButtonDash.setOnClickListener {
+            MaterialDialog(this).show {
+                title(R.string.Logout)
+                message(R.string.are_your_sure_logout)
+                positiveButton(R.string.logout) { dialog ->
+                    val a =  FirebaseAuth.getInstance()
+                    a.signOut()
+                    PrefUtils.setString(this@DashboardHome, PrefKeys.USER_ACC_TYPE, null)
+                    startActivity(Intent(this@DashboardHome, LoginActivity::class.java))
+                }
+                negativeButton(R.string.cancel) { dialog ->
+                    dismiss()
+                }
+            }
 
+        }
     }
 
     override fun onStart() {
